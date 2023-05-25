c0dea232856bcbf87fe9fd219791bddc71928a97
Ferrari <ferraridev18@gmail.com>
2023-05-25 15:46:12 -0300

    teste inserindo um comentário

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+    it("Deve renderizar o componente corretamente", () => {
+        render(<PostComment />);
+        expect(screen.getByText("Comentar")).toBeInTheDocument();
+    });
+
+    test("Deve adicionar 2 comentários no post", () => {
+        render(<PostComment />);
+        // const { debug } = render(<PostComment />);
+        fireEvent.change(screen.getByTestId("campo-comentario"), {
+            target: {
+                value: "Que miniatura foda.",
+            },
+        });
+        fireEvent.click(screen.getByTestId("btn-cadastrar"));
+        // debug();
+        expect(screen.getByText("Que miniatura foda.")).toBeInTheDocument();
     });
 });
